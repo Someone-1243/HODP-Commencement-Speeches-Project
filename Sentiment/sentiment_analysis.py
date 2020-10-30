afea06e92c97e7930e61bb68e426e70671e7580e
Someone-1243 <justinhye@gmail.com>
2020-10-30 19:07:51 -0400

    wiki

--- Sentiment/sentiment_analysis.py
+++ Sentiment/sentiment_analysis.py
@@ -47,7 +47,7 @@ theme_hodp = go.layout.Template(
     )
 )
 
-with open("sentiments.csv", "r", encoding="utf-8") as csvfile:
+with open("Sentiment/sentiments.csv", "r", encoding="utf-8") as csvfile:
     reader = csv.reader(csvfile)
     next(reader)
     li = []
@@ -57,9 +57,9 @@ with open("sentiments.csv", "r", encoding="utf-8") as csvfile:
         if row[0] != str(year):
             li.append(obj)
             if year == 2005 or year == 2002 or year == 2000:
-                year = year + 2
-            else:
                 year = year + 1
+                li.append([[year, 0, 'none',0,0,0]])
+            year = year + 1
             obj = []
         obj.append(row)
     li.append(obj)
@@ -82,7 +82,7 @@ with open("sentiments.csv", "r", encoding="utf-8") as csvfile:
         vals.append(neg/count)
         data.append(vals)
 
-pprint(data)
+# pprint(data)
 
 pos = []
 neutral = []
@@ -96,6 +96,19 @@ for i in range(len(data)):
     neutral.append(data[i][2])
     neg.append(data[i][3])
     summed.append(pos[i] + neutral[i])
+
+
+with open('Sentiment/speech-sentiment-avg.csv', "w", newline='', encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
+
+    writer.writerow(['Year','Positive','Neutral','Negative'])
+    for i in range(len(data)):
+        li = []
+        li.append(years[i])
+        li.append(pos[i])
+        li.append(neutral[i])
+        li.append(neg[i])
+        writer.writerow(li)
 
 fig = go.Figure(layout=go.Layout(barmode='stack'))
 
@@ -127,5 +140,114 @@ fig.update_layout(title="Sentiment Scores for Harvard Commencement Speeches",
                 yaxis={'title':{'text':'Sentiment Distribution'}}, 
                 legend={'title':{'text':'Sentiment'}},
                 template=theme_hodp)
+
+# fig.show()
+
+with open("Sentiment/Wiki/wiki_sentiments.csv", "r", encoding="utf-8") as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+    wiki_list = []
+    year = 2001
+    wiki_obj = []
+    for row in reader:
+        if row[0] != str(year):
+            wiki_list.append(wiki_obj)
+            year = year + 1
+            wiki_obj = []
+        wiki_obj.append(row[0:5])
+    wiki_list.append(wiki_obj)
+
+    wiki_data = [['2000',0,0,0]]
+    for year_entry in wiki_list:
+        pos = 0
+        neutral = 0
+        neg = 0
+        count = 0
+        vals = [year_entry[0][0]]
+        for item in year_entry:
+            count = count + 1
+            pos = pos + float(item[2])
+            neutral = neutral + float(item[3])
+            neg = neg + float(item[4])
+
+        vals.append(pos/count)
+        vals.append(neutral/count)
+        vals.append(neg/count)
+        wiki_data.append(vals)
+
+# pprint(wiki_data)
+
+wiki_data.append(['2020',0,0,0])
+
+wiki_pos = [0]
+wiki_neutral = [0]
+wiki_neg = [0]
+wiki_years = [2000]
+wiki_summed = [0]
+
+for i in range(len(wiki_data)):
+    wiki_years.append(int(wiki_data[i][0]))
+    wiki_pos.append(wiki_data[i][1])
+    wiki_neutral.append(wiki_data[i][2])
+    wiki_neg.append(wiki_data[i][3])
+    wiki_summed.append(wiki_pos[i] + wiki_neutral[i])
+
+fig2 = go.Figure(layout=go.Layout(barmode='stack'))
+
+fig2.add_trace(go.Bar(
+    x=wiki_years,
+    y=wiki_pos,
+    name='Positive score',
+    marker_color=primary_colors[2],
+))
+
+
+fig2.add_trace(go.Bar(
+    x=wiki_years,
+    y=wiki_neutral,
+    name='Neutral Score',
+    marker_color=primary_colors[1],
+))
+
+
+fig2.add_trace(go.Bar(
+    x=wiki_years,
+    y=wiki_neg,
+    name='Negative Score',
+    marker_color=primary_colors[0],
+))
+
+fig2.update_layout(title="Sentiment Scores per Year From Wikipedia", 
+                xaxis={'title':{'text':'Year'}}, 
+                yaxis={'title':{'text':'Sentiment Distribution'}}, 
+                legend={'title':{'text':'Sentiment'}},
+                template=theme_hodp)
+
+# fig2.show()
+
+pprint(wiki_data)
+
+dataframe = []
+for i in range(len(data)):
+    dataframe.append(data[i] + wiki_data[i])
+
+df = pd.DataFrame(dataframe, columns =['Years', 'Pos', 'Neutral', 'Neg', 'Wiki-Sentiment', 'Wiki-Pos', 'Wiki-Neutral', 'Wiki-Neg']) 
+df = df.drop([0,1,3,6,20])
+print(df) 
+
+
+fig = px.scatter(
+    df,
+    y='Neg',
+    x='Wiki-Neg',
+    color_discrete_sequence=primary_colors,
+    trendline='ols',
+    opacity=0.8
+)
+
+fig.update_layout(
+    title="Speech Sentiment vs. Year Summary Sentiment", 
+    template=theme_hodp
+)
 
 fig.show()
